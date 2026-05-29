1306098154f8ed3feda5f2b40e4e187d3404fbdf
Alekssandra Vojinovic <aleksandra@primer.com>
2026-05-29 12:40:31 +0200

    Izvrsene migracije za izmenu redova u tabeli

---
+++ backend/database/migrations/2026_05_29_103650_update_job_listings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('job_listings', function (Blueprint $table) {
+            $table->string('location')->nullable()->change();
+        $table->boolean('is_active')->default(true)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('job_listings', function (Blueprint $table) {
+            $table->string('location')->nullable(false)->change();
+            $table->boolean('is_active')->default(false)->change();
+        });
+    }
+};
