0c97361d9b302ccf6e826f375bdebdebca0f92d1
Nikola Vujovic <nikolavujovickv@gmail.com>
2026-06-20 16:10:24 +0200

    Pokrenut UserSeeder

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -5,16 +5,21 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserSeeder extends Seeder
 {
     use WithoutModelEvents;
     public function run(): void
     {
+        \DB::statement('SET FOREIGN_KEY_CHECKS=0');
         User::truncate();
+        \DB::statement('SET FOREIGN_KEY_CHECKS=1');
 
         User::factory(5)->create(['role' => 'job_seeker']);
         User::factory(3)->create(['role' => 'company']);
+
         User::create([
             'email'    => 'admin@karijera.rs',
             'password' => Hash::make('admin123'),
